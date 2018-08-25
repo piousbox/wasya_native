7e1e07894c368b2e7ad5b463e8d452f74dc34384
v p <piousbox@vs-MacBook-Pro.local>
2018-08-25 14:32:38 -0500

    grok

--- src/MainBanner.js
+++ src/MainBanner.js
@@ -4,7 +4,7 @@ import {
   Dimensions,
 } from 'react-native'
 import Image from 'react-native-scalable-image'
-import { Icon, } from 'react-native-elements'
+import { Icon, Card, } from 'react-native-elements'
 
 const bannerPath = './bg5.jpg'
 const banner = require(bannerPath)
@@ -12,6 +12,11 @@ const logoPath = './logo4.png'
 const logo = require(logoPath)
 
 const style = {
+  cardTitle: {
+    textAlign: 'center',
+    fontSize: 30,
+  },
+  cardDescription: {},
   text: { position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, 
     justifyContent: 'center',
     paddingLeft: 20,
@@ -48,28 +53,65 @@ class Header extends React.Component {
   }
 }
 
+class MyCard extends React.Component {
+  render () {
+    console.log('+++ myCard:', this.props)
+
+    return (
+      <View>
+        <Card image={require(this.props.imgPath)}>
+          <Text style={style.cardTitle} >{this.props.title}</Text>
+          <Divider />
+          <Text style={style.cardDescription} >{this.props.description}</Text>
+        </Card>
+      </View>
+      )
+  }
+}
+
 export default class MainBanner extends React.Component {
   render() {
+    const servicesCards = [
+      { title: "Ruby on Rails", imgPath: "./400x200/ruby.jpg", description: "Our framework for rapid prototyping and non-real-time applications is Ruby on Rails. We use it for constructing APIs, providing backend for websites, generating static websites, and as the tool for general scripting." },
+      /* { title: "", imgPath: "", description: "" },
+      { title: "", imgPath: "", description: "" },
+      { title: "", imgPath: "", description: "" },
+      { title: "", imgPath: "", description: "" },
+      { title: "", imgPath: "", description: "" },
+      { title: "", imgPath: "", description: "" },
+      { title: "", imgPath: "", description: "" }, */
+    ]
+    let ourServicesCards = []
+    servicesCards.forEach((item, idx) => {
+      ourServicesCards.push(<MyCard key={idx} title={item.title} description={item.description} imgPath={item.imgPath} />)
+    })
+
     return (
       <View >
-        <Image width={Dimensions.get('window').width} source={banner} />
-        <View style={style.text}>
-          <Text style={style.innerText} >Rapid Software Development</Text>
-          <Text style={style.innerText2}r >Using agile practices and the modern tech stack to achieve business goals</Text>
+        <View>
+          <Image width={Dimensions.get('window').width} source={banner} />
+          <View style={style.text}>
+            <Text style={style.innerText} >Rapid Software Development</Text>
+            <Text style={style.innerText2} >Using agile practices and the modern tech stack to achieve business goals</Text>
+          </View>
         </View>
-
+        
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} >
           <Image source={logo} style={{ justifyContent: 'center' }} />
         </View>
+
         <Divider />
         <Text style={style.p} >We are a software consulting firm that specializes in full-cycle web application development. We service startups and small local businesses that are focused on technology. We utilize modern best development practices and provide our clients with cost-effective and performant tools.</Text>
-        
+
         <Header>Services</Header>
+                
         <Text style={style.p} >We offer two parallel services to our clients. We provide technical solutions to high-tech startups and mid-sized companies. We offer complete software solutions, support of existing solutions, data migrations, feature implementation, iterative development, and training.  </Text>
         <Text style={style.p} >In this parallel, we perform business analysis, find critical issues & discovery across all areas of business (core value proposition, operations, sales, marketing, and finance) to propose quick wins and elimination of bottlenecks - how a manageable amount of effort can make the most impact on the business's bottom line. Additionally, we provide recommendations on sorporate culture, processes, and tools. High-impact business consulting to startups and companies in emerging markets.  </Text>
 
         <Header>Software Development</Header>
         <Text style={style.p} >We do web application development, integration with external services, deployments, automation, wireframing and prototyping, as well as monitoring and scaling of existing applications. We will also go refactoring/rewriting of an existing application and migrating it from old codebase to nice and shiny new paradigm.</Text>
+        
+        { ourServicesCards }
 
       </View>)
   }
